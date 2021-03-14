ffd995cdb2a305f9b453661fdd52e633596063bc
Benjamin <benjamin.karlsson@squeed.com>
2021-03-14 21:02:14 +0100

    poc with dark and light theme switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Theme, ThemeProvider } from "@emotion/react";
+import styled from "@emotion/styled";
+import React, { useState } from "react";
+import "./App.css";
+import logo from "./logo.svg";
+import { ReactComponent as Moon } from "./moon.svg";
+import { ReactComponent as Sun } from "./sun.svg";
+
+const lightTheme: Theme = {
+  type: "light",
+  color: {
+    primary: "#FBBF24",
+    text: "#1F2937",
+    contrastText: "#F3F4F6",
+    secondary: "#F9A8D4",
+    elevated: "#D1D5DB",
+    background: "#F3F4F6",
+  },
+};
+const darkTheme: Theme = {
+  type: "dark",
+  color: {
+    primary: "#8B5CF6",
+    text: "#F3F4F6",
+    contrastText: "#1F2937",
+    secondary: "#6EE7B7",
+    elevated: "#6B7280",
+    background: "#1F2937",
+  },
+};
+
+const StyledDiv = styled.div`
+  color: ${({ theme }) => theme.color.primary};
+`;
+
+const Primary = styled.span`
+  color: ${({ theme }) => theme.color.primary};
+`;
+const StyledButton = styled.button`
+  background: ${({ theme }) => theme.color.primary};
+  color: ${({ theme }) => theme.color.contrastText};
+`;
+const StyledMoon = styled(Moon)`
+  color: ${({ theme }) => theme.color.primary};
+`;
+const StyledSun = styled(Sun)`
+  color: ${({ theme }) => theme.color.primary};
+`;
+
+const StyledBackground = styled.div`
+  ${({ theme }) => `
+  background: ${theme.color.background}
+  `};
+`;
+
+const Paragraph = styled.p`
+  color: ${({ theme }) => theme.color.text};
+`;
 
 function App() {
+  const [theme, setTheme] = useState(lightTheme);
+
+  const onChangeTheme = () => {
+    const newTheme = theme.type === "light" ? darkTheme : lightTheme;
+    setTheme(newTheme);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <StyledBackground>
+        <header className="App-header">
+          {theme.type === "light" ? (
+            <StyledSun className="App-logo" />
+          ) : (
+            <StyledMoon className="App-logo" />
+          )}
+
+          <Paragraph>
+            Click the button to switch to{" "}
+            <Primary>{theme.type === "light" ? "light" : "dark"}</Primary> mode!
+          </Paragraph>
+
+          <StyledButton onClick={onChangeTheme}>Switch theme</StyledButton>
+          <StyledDiv>{theme.type} theme</StyledDiv>
+        </header>
+      </StyledBackground>
+    </ThemeProvider>
   );
 }
 
